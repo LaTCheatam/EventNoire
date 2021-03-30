5280647c0a3b25f74819b2493cc0700350dcca80
LaTCheatam <obsidyenmoon@gmail.com>
2021-03-30 11:22:27 -0700

    this is where Heroku refuses to deploy

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -13,38 +13,39 @@ const { requireAuth } = require('../../utils/auth.js');
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
-// GET /api/set-token-cookie (test)
-router.get('/set-token-cookie', asyncHandler(async (req, res) => {
-  const user = await User.findOne({
-      where: {
-        username: 'Demo-lition'
-      },
-    })
-  setTokenCookie(res, user);
-  return res.json({ user });
-}));
 
-// GET /api/restore-user (test)
-router.get(
-  '/restore-user',
-  restoreUser,
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
+// // GET /api/set-token-cookie (test)
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       },
+//     })
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
 
-// GET /api/require-auth (test)
-router.get(
-  '/require-auth',
-  requireAuth,
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
+// // GET /api/restore-user (test)
+// router.get(
+//   '/restore-user',
+//   restoreUser,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
 
-// router test
-router.post('/test', function(req, res) {
-  res.json({ requestBody: req.body });
-});
+// // GET /api/require-auth (test)
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// // router test
+// router.post('/test', function(req, res) {
+//   res.json({ requestBody: req.body });
+// });
 
 module.exports = router;
